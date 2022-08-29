a995b232f5aa2f6c53496962a12eea5b04b998eb
vreverberi <viviana.reverberi@gmail.com>
2022-08-29 11:48:56 -0300

    front

--- src/pages/NovedadesPage.js
+++ src/pages/NovedadesPage.js
@@ -14,7 +14,7 @@ const NovedadesPage = (props) => {
     useEffect(() => {
         const cargarNovedades = async () => {
             setLoading(true);
-            const response = await axios.get('http://localhost:3000/api/novedades');
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/novedades`);
             setNovedades(response.data);
             setLoading(false);
         };
